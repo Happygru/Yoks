5a13087c8848c011faf0b9540c5020aceebd72fc
happygru <happygru237@gmail.com>
2023-08-13 10:33:57 -0400

    fix

--- src/components/FleetCard.jsx
+++ src/components/FleetCard.jsx
@@ -13,7 +13,7 @@ const FleetCard = (props) => {
       className="p-8 bg-white rounded-md max-w-[370px] w-full flex flex-col gap-4 border border-[#dedede] shadow-lg cursor-pointer"
     >
       <h1 className="text-[var(--text-color)] font-semibold text-[20px]">
-        Compact Sedab Cars
+        Compact Sedan Cars
       </h1>
       <p className="text-[var(--text-color)] text-lg font-text">
         Sedan and it is powered by 1.4 litre naturally aspirated four-cylinder
