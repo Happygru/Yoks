fd963a56277a8cd012bce682db9dc9ab5b8a3cc2
happygru <happygru237@gmail.com>
2023-09-15 14:00:42 -0400

    deploy test

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,10 @@
-import { GOOGLE_API_KEY } from "./config";
+// import { GOOGLE_API_KEY } from "./config";
 
 export const getGPSPosition = async (address) => {
-  const result = await fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_API_KEY}`
-  ).then((response) => response.json());
-  const position = result.results[0].geometry.location;
-  return `${position.lat}, ${position.lng}`;
+  // const result = await fetch(
+  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_API_KEY}`
+  // ).then((response) => response.json());
+  // const position = result.results[0]?.geometry?.location;
+  // return `${position.lat}, ${position.lng}`;
+  return "5, -1";
 };
